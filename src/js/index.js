243b4a6025b2bb7a67f400b37ebaa750421be94c
Emma Grey <emgrey02@gmail.com>
2022-03-25 12:35:01 -0400

    Create scroll animation

---
+++ src/js/index.js
@@ -0,0 +1,37 @@
+const first = document.querySelector('.first');
+const second = document.querySelector('.second');
+const third = document.querySelector('.third');
+const about = document.querySelector('.about__container');
+const contact = document.querySelector('.contact__container');
+
+const className = 'in-view';
+
+first.classList.remove(className);
+second.classList.remove(className);
+third.classList.remove(className);
+about.classList.remove(className);
+contact.classList.remove(className);
+
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach((entry) => {
+        console.log(entry.target);
+        if (entry.isIntersecting) {
+            entry.target.classList.add(className) 
+            return;
+        }
+        entry.target.classList.remove(className);
+    });
+},
+{
+    threshold: .5
+});
+
+
+
+
+observer.observe(first);
+observer.observe(second);
+observer.observe(third);
+observer.observe(about);
+observer.observe(contact);
